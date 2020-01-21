1f509918a50584a5b2ee76d7dde4052b6e3e6a12
Andrew <45137330+syhnr@users.noreply.github.com>
2020-01-22 04:47:11 +0900

    Update commandlist.js
    Perepytal fail dynamicpage i commandlist

--- js/commandlist.js
+++ js/commandlist.js
@@ -1,33 +1,26 @@
-//Now json is in "../json/strings.js " :D
+var count = 0;
 
-function main() {
+commandtab.onmousedown = handler;
+function handler(e) {
+    const commandlist = document.getElementById('commandlist');
+        count++;
+        if(count == 2) {commandlist.innerHTML = ""; count = 0} else {;
+        const tab = document.querySelector('[href="#commands"]');
+        commandlist.innerHTML = json[0]["commandnav"];
+        }
+} 
+//It's dropdown
 
-	if(document.location.href.includes("#")) {
-		var anchor = document.location.href.split('/'),
-		tab = document.querySelector(`[href$=${anchor[anchor.length - 1]}]`);
-		document.getElementById('viewer').innerHTML = json[0][`${anchor[anchor.length - 1]}`];
-		document.getElementById('#title').innerHTML = anchor[anchor.length - 1].charAt(0).toUpperCase() + anchor[anchor.length - 1].slice(1);
-		try {
-			tab.classList.add("active"); //try because if you in command list script cant see tab
-		} catch {};
-	}
-
-}
-
-if ("onhashchange" in window)
-	window.onhashchange = function () {
-
-		var anchor = document.location.href.split('/'),
-		tab = document.querySelector(`[href$=${anchor[anchor.length - 1]}]`); //"href$" is https://drafts.csswg.org/selectors/#overview
-
-		document.querySelectorAll('a').forEach(
-			function _clear(e) {
-				e.classList.remove('active')
-			}
-		)
-		document.getElementById('#title').innerHTML = anchor[anchor.length - 1].charAt(0).toUpperCase() + anchor[anchor.length - 1].slice(1);
-		
-		tab.classList.add("active")
-
-		viewer.innerHTML = json[0][`${anchor[anchor.length - 1]}`];
-}
+var konami = ['38','38','40','40','37','39','37','39','66','65','13'], key = 0;
+  document.addEventListener('keydown', function(e) {
+    var requiredKey = konami[key];
+      if (e.keyCode == requiredKey) {
+      key++;
+      if (key == konami.length) {
+        document.getElementById('viewer').innerHTML = '<img src="https://i.redd.it/fs6z3gbyde241.jpg" alt="doggo">';
+        key = 0;
+      }
+    } else {
+      key = 0;
+    }
+});
